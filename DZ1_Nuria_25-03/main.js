601cae4c91670358a145a80f3114de7dc971a161
Nuria00004 <130464862+Nuria00004@users.noreply.github.com>
2023-05-24 13:30:43 +0600

    Add files via upload

--- DZ1_Nuria_25-03/main.js
+++ DZ1_Nuria_25-03/main.js
@@ -4,11 +4,20 @@ console.log(child)
  let positionY = 0
 
 const move = ()=> {
-    if(positionX <= 440 && positionY <= 0){
-        positionX += 16
-        child.style.left = `${positionX}px`
-        setTimeout(move, 100)
+    // child.style.marginLeft = 100 + "px"
+    // child.style.marginLeft = `${200}px`// интерпаляция
+    if(positionY < 450 && positionX === 0){
+          setTimeout(()=> {
+        positionY = positionY + 10
+        console.log(positionY)
+        child.style.marginLeft = `${positionY}px`
+
+        move()
+
+    },50)
     }
+
+
 }
 move()
 //////////////////////////////////
@@ -16,17 +25,26 @@ const input = document.getElementById('customerGmail')
 const btn = document.querySelector('.btn')
 const block = document.querySelector('.block')
 
-const regExp = /^[A-Za-z0-9\-\.\_]+\@[gmail]+\.[A-Za-z]{2,3}$/;
+const regExp = /^[A-Za-z0-9\-\.\_]+\@[gmail]+\.[A-Za-z]{2,3}$/ //$ - после этого ничего нельзя писать
+// ^ - должно начинаться с начала
+// $ - с конца
 
 btn.onclick =()=>{
     if(regExp.test(input.value)){
-        block.innerHTML = 'OK'
+        block.innerHTML = 'GREAT!'
         block.style.color = 'green'
     }else{(regExp.test(input.value))
-        block.innerHTML = 'NOT OK'
+        block.innerHTML = 'Invalid email format'
         block.style.color = 'red'
     }
 }
 
+//input.value - для инпута
+//innerHTML - для остальных тегов
 
+// if(positionX <= 440 && positionY <= 0){
+//         positionX += 16
+//         child.style.left = `${positionX}px`
+//         setTimeout(move, 100)
+//     }
 
--- DZ2_Nuria_25-03/main.js
+++ DZ2_Nuria_25-03/main.js
@@ -1,11 +1,11 @@
 const child = document.querySelector('.child')
-console.log(child)
- let positionX = 0
+
  let positionY = 0
+ let positionX = 0
 
 const move = ()=> {
     if(positionX <= 440 && positionY <= 0){
-        positionX += 16
+        positionX = positionX + 16
         child.style.left = `${positionX}px`
         setTimeout(move, 100)
     }else if(positionY <= 440 && positionX >= 440){
@@ -31,36 +31,43 @@ const btnStop = document.querySelector('.btn_stop')
 const btnClear = document.querySelector('.btn_clear')
 const btnResume = document.querySelector('.btn_resume')
 
-let i = 0
-let stop
+let count = 0
+let stop;
 
 
 btnStart.addEventListener('click', () => {
     stop = setInterval(() => {
-        i++
-        counter.innerHTML = i
+        count=count+1
+        counter.innerHTML = count
     }, 1000)
 })
+
 ////////////////////////
 btnStop.addEventListener('click', () => {
    clearInterval(stop)
 })
 ////////////////////////
 btnClear.addEventListener('click', () => {
-    i = 0
-    counter.innerHTML = i
+    count = 0
+    counter.innerHTML = count
     clearInterval(stop)
 })
 ////////////////////////
 btnResume.addEventListener('click', () => {
     stop = setInterval(() => {
-        i++
-        counter.innerHTML = i
+        count++
+        counter.innerHTML = count
     }, 1000)
 })
 
 
 
 
-
+// setTimeout(()=>{
+//     console.log("setTimeout")
+// },1000)
+//
+// setInterval(()=>{
+//     console.log("interval")
+// },1000)
 
